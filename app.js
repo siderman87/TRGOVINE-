2d77130e902b9ed0fa99398736bc1cc8a070ddd4
siderman87 <info.gregor.grm@gmail.com>
2026-06-17 11:41:10 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1118,6 +1118,8 @@ elements.filterMonth.addEventListener("change", applyMonthInput);
 ["filterFrom", "filterTo", "filterStore", "filterStatus"].forEach((id) => {
   elements[id].addEventListener("change", renderReports);
 });
+let state = loadState();
+state.activeUserId = localStorage.getItem(SESSION_KEY) || "";
 
 resetClosingForm();
 render();
